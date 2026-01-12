00c8267324642f5a8286483341de5668c4f697d5
jsaenz <jsaenz@hiberus.es>
2026-01-12 13:50:49 +0100

    Añadir FontAwesome para iconos en tablas

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,21 +5,19 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <meta name="description" content="SyncJornada - Gestión inteligente del tiempo laboral">
+        <link rel="icon" type="image/svg+xml" href="{{ asset('images/favicon.svg') }}">
 
         <title>{{ config('app.name', 'SyncJornada') }} - @yield('title', 'Dashboard')</title>
 
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
         <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@100..900&display=swap" rel="stylesheet" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body 
-        x-data="{ page: 'dashboard', 'loaded': true, 'darkMode': false, 'stickyMenu': false, 'sidebarToggle': false, 'scrollTop': false }"
-        x-init="
-            darkMode = JSON.parse(localStorage.getItem('darkMode'));
-            $watch('darkMode', value => localStorage.setItem('darkMode', JSON.stringify(value)))"
-        :class="{'dark bg-gray-900': darkMode === true}"
+        x-data="{ page: 'dashboard', 'loaded': true, 'stickyMenu': false, 'sidebarToggle': false, 'scrollTop': false }"
         class="font-sans antialiased"
     >
         @include('layouts.partials.preloader')
